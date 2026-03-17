98d0a4b333d3204ed826a7e4ea3b3a34f6caa7b3
Nando001145k <142845788+Nando001145k@users.noreply.github.com>
2026-03-16 21:02:18 -0300

    Add files via upload
    sem o calloc

--- Similaridade.cpp
+++ Similaridade.cpp
@@ -1,8 +1,6 @@
-
 #include <stdio.h>  
 #include <stdlib.h> 
 #include "Similaridade.h" 
-
 
 Matriz criarMatrizDensa(ListaCompras* dados) {
     Matriz mat;
@@ -11,7 +9,13 @@ Matriz criarMatrizDensa(ListaCompras* dados) {
 
     mat.valores = (int**) malloc(mat.linhas * sizeof(int*));
     for (int i = 0; i < mat.linhas; i++) {
-        mat.valores[i] = (int*) calloc(mat.colunas, sizeof(int));
+        // Usa malloc no lugar de calloc
+        mat.valores[i] = (int*) malloc(mat.colunas * sizeof(int));
+        
+        // Zera os elementos manualmente
+        for (int j = 0; j < mat.colunas; j++) {
+            mat.valores[i][j] = 0;
+        }
     }
 
     for (int i = 0; i < mat.linhas; i++) {
@@ -26,12 +30,16 @@ Matriz calcularIntersecao(Matriz* A) {
     Matriz I;
     I.linhas = A->linhas;   
     I.colunas = A->linhas;
-
-
     
     I.valores = (int**) malloc(I.linhas * sizeof(int*));
     for (int i = 0; i < I.linhas; i++) {
-        I.valores[i] = (int*) calloc(I.colunas, sizeof(int)); 
+        // Usa malloc no lugar de calloc
+        I.valores[i] = (int*) malloc(I.colunas * sizeof(int)); 
+        
+        // Zera os elementos manualmente (muito importante para o += funcionar depois)
+        for (int j = 0; j < I.colunas; j++) {
+            I.valores[i][j] = 0;
+        }
     }
 
     for (int i = 0; i < I.linhas; i++) {          
@@ -48,7 +56,6 @@ MatrizSimilaridade calcularSimilaridade(Matriz* intersecao, ListaCompras* dados)
     MatrizSimilaridade S;
     S.n = intersecao->linhas;
     S.valores = (float**) malloc(S.n * sizeof(float*));
-
 
     for (int i = 0; i < S.n; i++) {
         S.valores[i] = (float*) malloc(S.n * sizeof(float));
